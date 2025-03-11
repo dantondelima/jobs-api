5b7a6a9493bb34ff7635a5e94a7273076f63be30
Danton de Lima <dantondelima.contato@gmail.com>
2025-03-11 18:34:59 -0300

    chore(Docker): add dockerfile and compose

---
+++ docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+if [ ! -f "./artisan" ]; then
+    echo "Invalid directory: $PWD"
+    exit 1
+fi
+
+if [ ! -f "./vendor/autoload.php" ]; then
+    printf "\e[35mStarting up container for the first time\e[0m\n"
+    printf "\n"
+    printf "\e[35mRunning composer install...\e[0m\n"
+    composer install
+    printf "\e[35mRunning migrations...\e[0m\n"
+    php artisan migrate --seed
+fi
+
+exec "$@"
